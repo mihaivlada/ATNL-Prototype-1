c33d56e7b4c7cb786699c85aea6304d9aa078d25
Vlada Mihai <vlada.mihai.catalin@gmail.com>
2026-07-28 23:22:08 +0300

    Camera flow; Colliders; Good lighting (#7)
    * removed duplicates
    
    * commit random
    
    * Camera flow; Colliders; Good lighting

---
+++ Assets/Scripts/Helper.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+
+// Aici punem membrii si metode helper care pot fi accesate de oriunde in proiect
+public static class Helper
+{
+    public static float playerSpeed = 5f;
+}
