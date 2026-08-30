646949c04576fe9c4fe64c3e8539b83139dc664a
Vlada Mihai <vlada.mihai.catalin@gmail.com>
2026-08-30 22:59:33 +0300

    more narrow amplitude of rotation on the X axis; light slowly fades in and out 10 seconds at the begining and at the end of the day

--- Assets/Scripts/WeatherAndEnvironment/DayAndNightCycle.cs
+++ Assets/Scripts/WeatherAndEnvironment/DayAndNightCycle.cs
@@ -4,23 +4,30 @@ using UnityEngine;
 [RequireComponent(typeof(Light))]
 public class DayAndNightCycle : MonoBehaviour
 {
-    [SerializeField] private float dayDuration;
-    [SerializeField] private float nightDuration;
-
-    private Light light;
-
+    [SerializeField] private float dayDurationInSeconds;
+    [SerializeField] private float nightDurationInSeconds;
+    [SerializeField] private float lightIntensityTransitionTimeInSeconds;
     // between 0 and 1
     [SerializeField] private float dayProgress;
 
+    private Light light;
+
     private bool isDay;
 
-    private const int sunAngleYAxis = 75;
-    private const int sunAngleXAxis = 75;
+    private const int startingAngleOffsetX = 75;
+    private const int startingAngleOffsetY = 75;
+
+    private const float startingAngleX = Mathf.PI / 8;
+    private const float rotationAmplitudeX = 3 * Mathf.PI / 4;
+
+    private const float startingAngleY = 3 * Mathf.PI / 2;
+    private const float rotationAmplitudeY = Mathf.PI;
 
     void Start()
     {
-        dayDuration = 8f * 60;
-        nightDuration = 7f * 60;
+        dayDurationInSeconds = 8f * 60;
+        nightDurationInSeconds = 7f * 60;
+        lightIntensityTransitionTimeInSeconds = 10f / dayDurationInSeconds;
 
         // start of the day
         dayProgress = 0;
@@ -28,8 +35,8 @@ public class DayAndNightCycle : MonoBehaviour
 
         // sped up for testing
         // remove in the future
-        //dayDuration /= 80;
-        //nightDuration /= 80;
+        //dayDurationInSeconds /= 40;
+        //nightDurationInSeconds /= 80;
 
         light = GetComponent<Light>();
     }
@@ -38,7 +45,14 @@ public class DayAndNightCycle : MonoBehaviour
     {
         if (isDay) 
         {
-            dayProgress += Time.deltaTime / dayDuration;
+            dayProgress += Time.deltaTime / dayDurationInSeconds;
+
+            light.intensity = Mathf.Clamp01(
+                Mathf.Min(
+                    dayProgress / lightIntensityTransitionTimeInSeconds,
+                    (1f - dayProgress) / lightIntensityTransitionTimeInSeconds
+                )
+            );
         }
 
         // end of the day, resets back to start
@@ -51,14 +65,14 @@ public class DayAndNightCycle : MonoBehaviour
         }
 
         transform.rotation = Quaternion.Euler(
-            sunAngleXAxis * Mathf.Sin(Mathf.PI * dayProgress), 
-            sunAngleYAxis * Mathf.Sin((3 * Mathf.PI / 2) + (Mathf.PI * dayProgress)), 
+            startingAngleOffsetX * Mathf.Sin(startingAngleX + (rotationAmplitudeX * dayProgress)), 
+            startingAngleOffsetY * Mathf.Sin(startingAngleY + (rotationAmplitudeY * dayProgress)), 
             0);
     }
 
     IEnumerator WaitForNightTime()
     {
-        yield return new WaitForSeconds(nightDuration);
+        yield return new WaitForSeconds(nightDurationInSeconds);
         TurnLightOn();
     }
 
